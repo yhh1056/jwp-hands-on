688f7770aeb01eefe21d444a6e807f87d3b60859
yhh1056 <58363663+yhh1056@users.noreply.github.com>
2022-09-07 17:56:12 +0900

    Cache 실습 (#1)
    * 캐시 헤더 설정 (private, no-cache)
    
    * 압축 크기 설정
    
    * etag 설정
    
    * 정적 파일 캐시 버스팅

--- cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
+++ cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
@@ -1,12 +1,24 @@
 package com.example.etag;
 
+import com.example.version.ResourceVersion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @Configuration
 public class EtagFilterConfiguration {
 
-//    @Bean
-//    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
-//        return null;
-//    }
+    @Autowired
+    ResourceVersion version;
+
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean
+                = new FilterRegistrationBean<>(new ShallowEtagHeaderFilter());
+        filterRegistrationBean.addUrlPatterns("/etag", "/resources/" + version.getVersion() +"/js/index.js");
+        filterRegistrationBean.setName("etagFilter");
+        return filterRegistrationBean;
+    }
 }
