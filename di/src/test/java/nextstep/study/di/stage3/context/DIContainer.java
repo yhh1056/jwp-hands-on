ecde0aeb8f7c892ba03938bab7a9b1ee6aa67a91
yhh1056 <58363663+yhh1056@users.noreply.github.com>
2022-10-11 23:10:02 +0900

    Di 실습 (#6)
    * step3
    
    * step4

--- di/src/test/java/nextstep/study/di/stage3/context/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage3/context/DIContainer.java
@@ -1,6 +1,12 @@
 package nextstep.study.di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Set;
+import java.util.stream.Collectors;
+import nextstep.study.ConsumerWrapper;
+import nextstep.study.FunctionWrapper;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,11 +16,36 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        beans.forEach(this::setFields);
     }
 
+    private static Set<Object> createBeans(final Set<Class<?>> classes) {
+        return classes.stream()
+                .map(FunctionWrapper.apply(Class::getDeclaredConstructor))
+                .peek(constructor -> constructor.setAccessible(true))
+                .map(FunctionWrapper.apply(Constructor::newInstance))
+                .collect(Collectors.toSet());
+    }
+
+    private void setFields(final Object bean) {
+        for (Field field : bean.getClass().getDeclaredFields()) {
+            Class<?> type = field.getType();
+            field.setAccessible(true);
+
+            beans.stream()
+                    .filter(type::isInstance)
+                    .forEach(ConsumerWrapper.accept(matchBean -> field.set(bean, matchBean)));
+        }
+    }
+
+    // 빈 컨테이너(DI)에서 관리하는 빈을 찾아서 반환한다.
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return beans.stream()
+                .filter(aClass::isInstance)
+                .findFirst()
+                .map(bean -> (T) bean)
+                .orElseThrow(() -> new IllegalArgumentException("bean을 찾을 수 없습니다."));
     }
 }
